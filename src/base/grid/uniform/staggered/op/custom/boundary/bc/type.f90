db9e0338ade9db6eeed4d520e3a053fb4376c107
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-06 20:03:50 +0900

    added function to determaine boundary type

--- src/base/grid/uniform/staggered/op/custom/boundary/bc/type.f90
+++ src/base/grid/uniform/staggered/op/custom/boundary/bc/type.f90
@@ -4,11 +4,14 @@
 !第2種（Neumann）境界条件を表す定数が定義される．
 !
 module grid_uniform_staggered_op_custom_bc_type
+    use, intrinsic :: iso_fortran_env
     use, intrinsic :: iso_c_binding
     implicit none
     private
     public :: boundary_Dirichlet, &
               boundary_Neumann
+    public :: is_Dirichlet_boundary, &
+              is_Neumann_boundary
 
     enum, bind(c)
         enumerator :: boundary_Dirichlet = 1
@@ -17,4 +20,26 @@ module grid_uniform_staggered_op_custom_bc_type
             !! 第2種（Neumann）境界条件を表す定数
     end enum
 
+contains
+    !| 境界の種類がDirichlet境界条件の場合に`.true.`を，
+    !そうでない場合に`.false.`を返す．
+    logical function is_Dirichlet_boundary(boundary_type)
+        implicit none
+
+        integer(int32), intent(in) :: boundary_type
+            !! 境界の種類
+
+        is_Dirichlet_boundary = (boundary_type == boundary_Dirichlet)
+    end function is_Dirichlet_boundary
+
+    !| 境界の種類がNeumann境界条件の場合に`.true.`を，
+    !そうでない場合に`.false.`を返す．
+    logical function is_Neumann_boundary(boundary_type)
+        implicit none
+
+        integer(int32), intent(in) :: boundary_type
+            !! 境界の種類
+
+        is_Neumann_boundary = (boundary_type == boundary_Neumann)
+    end function is_Neumann_boundary
 end module grid_uniform_staggered_op_custom_bc_type
