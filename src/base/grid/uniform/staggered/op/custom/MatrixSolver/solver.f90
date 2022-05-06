3680ae93844283be3a8dee4bec274bb0e87c1b3a
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-06 21:45:41 +0900

    added solver module

---
+++ src/base/grid/uniform/staggered/op/custom/MatrixSolver/solver.f90
@@ -0,0 +1,11 @@
+!| 2次元Staggered格子上で構築される連立方程式を
+!一括して取り扱うために`use`するモジュール
+module grid_uniform_staggered_op_custom_solver
+    use :: grid_uniform_staggered_op_custom_solver_Ax_adt
+    use :: grid_uniform_staggered_op_custom_solver_Ax_laplacian
+    use :: grid_uniform_staggered_op_custom_solver_AxEqB
+    use :: grid_uniform_staggered_op_custom_solver_results
+    use :: grid_uniform_staggered_op_custom_solver_inverse
+    use :: grid_uniform_staggered_op_custom_solver_until
+    public
+end module grid_uniform_staggered_op_custom_solver
