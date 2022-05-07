cdeb7e8d61121c3ebf1338e174ff5e20528c0089
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-07 11:28:45 +0900

    added flop module

---
+++ src/flop/flop.f90
@@ -0,0 +1,15 @@
+!| Flopを構成するモジュールを
+!一括して`use`するためのモジュール
+module flop
+    use :: time_axis
+    use :: discreteTime
+
+    use :: space_axis
+    use :: space_Cartesian
+
+    use :: grid_uniform_staggered_2d
+    use :: grid_uniform_staggered_variables
+    use :: grid_uniform_staggered_operators
+
+    public
+end module flop
