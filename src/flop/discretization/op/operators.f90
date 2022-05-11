c608029ea42ef9b219d409aa5c502ec181f9b478
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-12 00:52:58 +0900

    added discretization opeartor module

---
+++ src/flop/discretization/op/operators.f90
@@ -0,0 +1,8 @@
+!>時空間離散化に関するオペレータを一括して`use`するためのモジュール
+module discretization_op
+    use :: discretization_op_divide
+    use :: discretization_op_into
+    use :: discretization_op_cells
+    use :: discretization_op_intervals
+    public
+end module discretization_op
