2a096eaf0a5a664508c1ff9a449d38617fb98486
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-11 21:20:53 +0900

    added `as` operator to construct formatted writer

---
+++ src/flop/grid/uniform/staggered/op/io/as/as.f90
@@ -0,0 +1,57 @@
+!>物理量出力時の書式整形に関係する手続を提供する．
+!>
+!>手続には，物理量と書式から物理量の出力子を作成する手続が含まれる．
+!>
+module grid_uniform_staggered_op_io_as
+    use :: grid_uniform_staggered_vars_scalar_2d
+    use :: grid_uniform_staggered_vars_vector_2d
+    use :: grid_uniform_staggered_op_io_format
+    use :: grid_uniform_staggered_op_io_vars_scalar_writer_csv
+    use :: grid_uniform_staggered_op_io_vars_vector_writer_csv
+    implicit none
+    private
+    public :: operator(.as.)
+
+    !>ユーザ定義演算子`.as.`を定義するインタフェース
+    interface operator(.as.)
+        procedure :: as_scr2d_csvfmt
+        procedure :: as_vec2d_csvfmt
+    end interface
+
+contains
+    !>スカラ量をCSV形式に整形する出力子を返す．
+    function as_scr2d_csvfmt(scr, csv_fmt) result(new_scr_writer)
+        implicit none
+        !&<
+        type(scalar_2d_type)    , intent(in) :: scr
+            !! 出力されるスカラ量
+        type(csv_format_type)   , intent(in) :: csv_fmt
+            !! CSV形式指標
+        !&>
+        type(scalar_2d_csv_writer_type) :: new_scr_writer
+            !! スカラ量のCSV形式出力子
+
+        new_scr_writer%scr = scr
+
+        return
+        if (same_type_as(csv_fmt, csv_fmt)) continue ! 変数未使用警告の抑制
+    end function as_scr2d_csvfmt
+
+    !>ベクトル量をCSV形式に整形する出力子を返す．
+    function as_vec2d_csvfmt(vec, csv_fmt) result(new_vec_writer)
+        implicit none
+        !&<
+        type(vector_2d_type)    , intent(in) :: vec
+            !! 出力されるベクトル量
+        type(csv_format_type)   , intent(in) :: csv_fmt
+            !! CSV形式指標
+        !&>
+        type(vector_2d_csv_writer_type) :: new_vec_writer
+            !! ベクトル量のCSV形式出力子
+
+        new_vec_writer%vec = vec
+
+        return
+        if (same_type_as(csv_fmt, csv_fmt)) continue ! 変数未使用警告の抑制
+    end function as_vec2d_csvfmt
+end module grid_uniform_staggered_op_io_as
