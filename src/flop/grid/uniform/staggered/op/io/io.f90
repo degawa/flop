debeba18228a4060f9629be51c46bab2dcc7d2ac
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-11 21:23:12 +0900

    added io module

---
+++ src/flop/grid/uniform/staggered/op/io/io.f90
@@ -0,0 +1,10 @@
+!| 物理量のIOに関わる型と演算子を一括して`use`するためのモジュール
+module grid_uniform_staggered_op_io
+    use :: grid_uniform_staggered_op_io_vars_vector_writer_csv
+    use :: grid_uniform_staggered_op_io_vars_scalar_writer_csv
+    use :: grid_uniform_staggered_op_io_format
+    use :: grid_uniform_staggered_op_io_as
+    use :: grid_uniform_staggered_op_io_unit
+    use :: grid_uniform_staggered_op_io_to
+    public
+end module grid_uniform_staggered_op_io
