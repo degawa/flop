9a871f59dcc79f8073616b85895fd7bc5f9d0568
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-14 23:55:01 +0900

    fixed module name

--- src/flop/discretization/vars/variables.f90
+++ src/flop/discretization/vars/variables.f90
@@ -1,6 +1,6 @@
 !>時空間離散化に関する型を一括して`use`するためのモジュール
-module discretization_vars
+module discretization_variables
     use :: discretization_vars_cells
     use :: discretization_vars_intervals
     public
-end module discretization_vars
+end module discretization_variables
