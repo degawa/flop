ed0a6008d433786599a1f431f2659b0409a12b5b
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-14 22:31:01 +0900

    added ud for representing discretize parameters

--- src/flop/discretization/op/cells/cells.f90
+++ src/flop/discretization/op/cells/cells.f90
@@ -1,23 +1,37 @@
-!>空間離散化に関する手続を提供する．
+!>空間離散化に関する派生型や手続を提供する．
 !>
-!>手続には，離散化する空間にセル数の情報を渡すための手続が含まれる．
+!>派生型には，セル数をまとめた派生型が含まれる．
+!>
+!>手続には，セル数をまとめた派生型のコンストラクタが含まれる．
 !>
 module discretization_op_cells
     use, intrinsic :: iso_fortran_env
     implicit none
     private
-    public :: cells
+    public :: Cells
+
+    !>セル数をまとめた型．
+    type, public :: cells_type
+        integer(int32), allocatable :: num_cells(:)
+            !! セル数
+    end type cells_type
+
+    !>セル数をまとめた型のコンストラクタを`Cells`と呼べるようにするための
+    !>インタフェース
+    interface Cells
+        procedure :: construct_cells
+    end interface
 
 contains
-    !>各軸のセル数を受け取り，セル数をまとめた配列を返す．
-    function cells(num_cells) result(new_cells)
+    !>各軸のセル数を受け取り，セル数をまとめた型を返す．
+    function construct_cells(num_cells) result(new_cells)
         implicit none
         integer(int32), intent(in) :: num_cells(:)
             !! 各軸方向のセル数
 
-        integer(int32), allocatable :: new_cells(:)
-            !! 各軸方向のセル数
+        type(cells_type) :: new_cells
+            !! 各軸方向のセル数をまとめた型
 
-        new_cells = num_cells
-    end function cells
+        new_cells%num_cells = num_cells
+    end function construct_cells
 end module discretization_op_cells
--- src/flop/discretization/op/intervals/intervals.f90
+++ src/flop/discretization/op/intervals/intervals.f90
@@ -1,24 +1,51 @@
-!>時空間離散化に関する手続を提供する．
+!>時空間離散化に関する派生型や手続を提供する．
 !>
-!>手続には，離散化する軸に分割幅の情報を渡すための手続が含まれる．
+!>派生型には，微小区間の幅を表現する派生型が含まれる．
+!>
+!>手続には，微小区間の幅を表現する派生型のコンストラクタが含まれる．
 !>
 module discretization_op_intervals
     use, intrinsic :: iso_fortran_env
     implicit none
     private
-    public :: intervals
+    public :: Intervals
+
+    !>微小区間の間隔を表現する派生型
+    type, public :: intervals_type
+        real(real64), allocatable :: intervals(:)
+            !! 微小区間の間隔
+    end type intervals_type
+
+    !>微小区間の間隔を表現する派生型に対するコンストラクタを
+    !>`Intervals`と呼べるようにするためのインタフェース
+    interface Intervals
+        procedure :: construct_interval
+    end interface
 
 contains
-    !>分割後の微小空間の幅を受け取り，そのまま返す．
-    function intervals(interval) result(new_interval)
+    !>分割後の微小空間の幅を受け取り，微小区間の幅を返す．
+    function construct_interval(interval) result(new_intervals)
         implicit none
 
         real(real64), intent(in) :: interval
             !! 分割後の微小空間の幅
 
-        real(real64) :: new_interval
+        type(intervals_type) :: new_intervals
             !! 分割後の微小空間の幅
 
-        new_interval = interval
-    end function intervals
+        allocate (new_intervals%intervals(1), source=interval)
+    end function construct_interval
+
+    !>分割後の微小空間の幅を受け取り，そのまま返す．
+    function construct_intervals(intervals) result(new_intervals)
+        implicit none
+
+        real(real64), intent(in) :: intervals(:)
+            !! 分割後の微小空間の幅
+
+        type(intervals_type) :: new_intervals
+            !! 分割後の微小空間の幅
+
+        new_intervals%intervals = intervals
+    end function construct_intervals
 end module discretization_op_intervals
