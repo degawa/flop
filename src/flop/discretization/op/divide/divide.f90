0e922784198cfc1c45b81db3a02e91bba3e35e3c
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-05-14 23:57:20 +0900

    modified behavior of `into` and `divide` operators

--- src/flop/discretization/op/divide/divide.f90
+++ src/flop/discretization/op/divide/divide.f90
@@ -1,61 +1,50 @@
-!>時空間離散化に関する派生型，手続を提供する．
+!>時空間離散化に関する手続を提供する．
 !>
-!>派生型には，空間離散化に用いる空間情報を持つ型，
-!>および時間離散化に用いる時間情報を持つ型が含まれる．
-!>
-!>手続には，空間離散化に用いる空間情報を持つ型
-!>および時間離散化に用いる時間情報を持つ型のコンストラクタが含まれる．
+!>手続には，空間情報を用いて格子に離散化する手続や，
+!>時間情報を用いて時間離散化を行う手続が含まれる．
 !>
 module discretization_op_divide
     use, intrinsic :: iso_fortran_env
-    use :: space_vars_Cartesian
-    use :: time_vars_axis
+    use :: grid_uniform_staggered_2d
+    use :: discreteTime_vars_discreteTime
+    use :: discretization_op_vars_space_intoCells
+    use :: discretization_op_vars_time_intoIntervals
     implicit none
     private
     public :: operator(.divide.)
 
-    !>空間離散化に用いる空間情報を持つ型．
-    type, public :: space_container_type
-        type(Cartesian_2d_type) :: space
-            !! 離散化する空間座標系
-    end type space_container_type
-
-    !>時間離散化に用いる時間軸情報を持つ型．
-    type, public :: time_container_type
-        type(time_axis_type) :: time
-            !! 離散化する時間軸
-    end type time_container_type
-
     !>ユーザ定義演算子`.divide.`を定義するインタフェース
     interface operator(.divide.)
-        procedure :: construct_space_container
-        procedure :: construct_time_container
+        procedure :: divide_space_into_cells
+        procedure :: divide_time_into_intervals
     end interface
 
 contains
-    !>離散化に用いる空間情報を持つ型を構築して返す．
-    function construct_space_container(space) result(space_container)
+    !>空間情報を用いて空間を離散化し，格子を返す．
+    function divide_space_into_cells(space_into_cells) result(new_grid)
         implicit none
+        !&<
+        type(space_into_cells_type), intent(in) :: space_into_cells
+            !! 離散化する空間情報
+        !&>
+        type(staggered_uniform_grid_2d_type) :: new_grid
+            !! 構築されるスタガード格子
 
-        type(Cartesian_2d_type), intent(in) :: space
-            !!
+        integer(int32), allocatable :: num_points(:)
+        num_points = space_into_cells%cells%num_cells + 1
 
-        type(space_container_type) :: space_container
-            !!
+        call new_grid%construct(space_into_cells%space, num_points)
+    end function divide_space_into_cells
 
-        space_container%space = space
-    end function construct_space_container
-
-    !>離散化に用いる時間軸情報を持つ型を構築して返す．
-    function construct_time_container(time) result(time_container)
+    !>時間情報を用いて時間を離散化し，離散化された時間情報を返す．
+    function divide_time_into_intervals(time_into_interval) result(new_delta)
         implicit none
+        type(time_into_intervals_type), intent(in) :: time_into_interval
+            !! 離散化する時間情報
+        type(discrete_time_type) :: new_delta
+            !! 離散化された時間
 
-        type(time_axis_type), intent(in) :: time
-            !!
-
-        type(time_container_type) :: time_container
-            !!
-
-        time_container%time = time
-    end function construct_time_container
+        call new_delta%construct_by_time_interval(time_into_interval%time, &
+                                                  time_into_interval%intervals%intervals(1))
+    end function divide_time_into_intervals
 end module discretization_op_divide
--- src/flop/discretization/op/into/into.f90
+++ src/flop/discretization/op/into/into.f90
@@ -1,54 +1,56 @@
 !>時空間離散化に関する手続を提供する．
 !>
-!>手続には，空間情報を用いて格子に離散化する手続や，
-!>時間情報を用いて時間離散化を行う手続が含まれる．
+!>手続には，空間座標系と各軸方向のセル数をまとめる手続，
+!>および時間軸と時間方向の微小区間の幅をまとめる手続が含まれる．
 !>
 module discretization_op_into
-    use, intrinsic :: iso_fortran_env
-    use :: discretization_op_divide
-    use :: grid_uniform_staggered_2d
-    use :: discreteTime_vars_discreteTime
+    use :: space_vars_Cartesian
+    use :: time_vars_axis
+    use :: discretization_vars_cells
+    use :: discretization_vars_intervals
+    use :: discretization_op_vars_space_intoCells
+    use :: discretization_op_vars_time_intoIntervals
     implicit none
     private
     public :: operator(.into.)
 
     !>ユーザ定義演算子`.into.`を定義するインタフェース
     interface operator(.into.)
-        procedure :: construct_grid
-        procedure :: construct_discrete_time
+        procedure :: construct_space_into_cells
+        procedure :: construct_time_into_intervals
     end interface
 
 contains
-    !>空間情報を用いて空間を離散化し，格子を返す．
-    function construct_grid(space_contaier, num_cells) result(new_grid)
+    !>空間情報とセル数をまとめた型を返す．
+    function construct_space_into_cells(space, cells) result(new_space_into_cells)
         implicit none
         !&<
-        type(space_container_type)  , intent(in) :: space_contaier
-            !! 離散化する空間情報
-        integer(int32)              , intent(in) :: num_cells(:)
+        type(Cartesian_2d_type) , intent(in) :: space
+            !! 離散化する空間座標系
+        type(cells_type)        , intent(in) :: cells
             !! 各軸方向のセル数
         !&>
-        type(staggered_uniform_grid_2d_type) :: new_grid
-            !! 構築されるスタガード格子
+        type(space_into_cells_type) :: new_space_into_cells
+            !! 空間情報とセル数をまとめた型
 
-        integer(int32), allocatable :: num_points(:)
-        num_points = num_cells + 1
+        new_space_into_cells%space = space
+        allocate (new_space_into_cells%cells%num_cells, source=cells%num_cells)
+    end function construct_space_into_cells
 
-        call new_grid%construct(space_contaier%space, num_points)
-    end function construct_grid
-
-    !>時間情報を用いて時間を離散化し，離散化された時間情報を返す．
-    function construct_discrete_time(time_container, interval) result(new_delta)
+    !>時間軸と微小区間の幅をまとめた型を返す．
+    function construct_time_into_intervals(time, intervals) result(new_time_into_interval)
         implicit none
         !&<
-        type(time_container_type)   , intent(in) :: time_container
-            !! 離散化する時間情報
-        real(real64)                , intent(in) :: interval
-            !! 計算時間間隔
+        type(time_axis_type), intent(in) :: time
+            !! 離散化する時間軸
+        type(intervals_type), intent(in) :: intervals
+            !! 時間軸の微小区間の幅
         !&>
-        type(discrete_time_type) :: new_delta
-            !! 離散化された時間
+        type(time_into_intervals_type) :: new_time_into_interval
+            !! 時間軸と微小区間の幅をまとめた型
 
-        call new_delta%construct(time_container%time, interval)
-    end function construct_discrete_time
+        new_time_into_interval%time = time
+        allocate (new_time_into_interval%intervals%intervals, &
+                  source=intervals%intervals)
+    end function construct_time_into_intervals
 end module discretization_op_into
