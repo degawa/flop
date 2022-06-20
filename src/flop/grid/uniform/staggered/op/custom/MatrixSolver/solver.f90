42376c33dbce23a5b9424b3249ff34c1209e5991
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-06-20 17:06:54 +0900

    added module to use Ax

--- src/flop/grid/uniform/staggered/op/custom/MatrixSolver/solver.f90
+++ src/flop/grid/uniform/staggered/op/custom/MatrixSolver/solver.f90
@@ -1,8 +1,7 @@
 !>2次元Staggered格子上で構築される連立方程式を
 !>一括して取り扱うために`use`するモジュール
 module grid_uniform_staggered_op_custom_solver
-    use :: grid_uniform_staggered_op_custom_solver_vars_Ax_adt
-    use :: grid_uniform_staggered_op_custom_solver_vars_Ax_laplacian
+    use :: grid_uniform_staggered_op_custom_solver_vars_Ax
     use :: grid_uniform_staggered_op_custom_solver_vars_AxEqB
     use :: grid_uniform_staggered_op_custom_solver_vars_solver
     use :: grid_uniform_staggered_op_custom_solver_vars_solver_spec
---
+++ src/flop/grid/uniform/staggered/op/custom/MatrixSolver/vars/Ax/Ax.f90
@@ -0,0 +1,6 @@
+!>連立方程式の左辺\(\boldsymbol{Ax}\)に関するモジュールを一括して`use`するためのモジュール
+module grid_uniform_staggered_op_custom_solver_vars_Ax
+    use :: grid_uniform_staggered_op_custom_solver_vars_Ax_adt
+    use :: grid_uniform_staggered_op_custom_solver_vars_Ax_laplacian
+    public
+end module grid_uniform_staggered_op_custom_solver_vars_Ax
