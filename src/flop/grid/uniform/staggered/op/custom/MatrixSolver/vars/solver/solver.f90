4f10f7e9d526c62d97fe8caae6e3f59fa67250bd
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-06-20 16:42:52 +0900

    added module to use liner solvers

---
+++ src/flop/grid/uniform/staggered/op/custom/MatrixSolver/vars/solver/solver.f90
@@ -0,0 +1,6 @@
+!>連立一次方程式の求解に関するモジュールを一括して`use`するためのモジュール
+module grid_uniform_staggered_op_custom_solver_vars_solver
+    use :: grid_uniform_staggered_op_custom_solver_vars_solver_adt
+    use :: grid_uniform_staggered_op_custom_solver_vars_solver_lap_SOR
+    public
+end module grid_uniform_staggered_op_custom_solver_vars_solver
