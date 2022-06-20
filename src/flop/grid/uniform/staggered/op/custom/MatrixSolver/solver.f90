81a17b5e7fe9389d4b6e2e519c13af74580af8eb
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-06-20 16:56:59 +0900

    updated solver module

--- src/flop/grid/uniform/staggered/op/custom/MatrixSolver/solver.f90
+++ src/flop/grid/uniform/staggered/op/custom/MatrixSolver/solver.f90
@@ -2,11 +2,14 @@
 !>一括して取り扱うために`use`するモジュール
 module grid_uniform_staggered_op_custom_solver
     use :: grid_uniform_staggered_op_custom_solver_vars_Ax_adt
-    use :: grid_uniform_staggered_op_custom_solver_Ax_vars_laplacian
+    use :: grid_uniform_staggered_op_custom_solver_vars_Ax_laplacian
     use :: grid_uniform_staggered_op_custom_solver_vars_AxEqB
+    use :: grid_uniform_staggered_op_custom_solver_vars_solver
+    use :: grid_uniform_staggered_op_custom_solver_vars_solver_spec
     use :: grid_uniform_staggered_op_custom_solver_results
     use :: grid_uniform_staggered_op_custom_solver_inverse
     use :: grid_uniform_staggered_op_custom_solver_until
     use :: grid_uniform_staggered_op_custom_solver_with
+    use :: grid_uniform_staggered_op_custom_solver_using
     public
 end module grid_uniform_staggered_op_custom_solver
