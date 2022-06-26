46eb08ded6b122ff45f70eafb1b2ab278d86ea98
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-06-26 18:40:31 +0900

    added new csv output controller

--- src/flop/grid/uniform/staggered/op/io/to/to.f90
+++ src/flop/grid/uniform/staggered/op/io/to/to.f90
@@ -15,6 +15,8 @@ module grid_uniform_staggered_op_io_to
     interface operator(.to.)
         procedure :: set_unit_number_to_scr2dcsvwriter
         procedure :: set_unit_number_to_vec2dcsvwriter
+        procedure :: set_unit_number_from_filename_to_scr2dcsvwriter
+        procedure :: set_unit_number_from_filename_to_vec2dcsvwriter
     end interface
 
 contains
@@ -49,4 +51,38 @@ contains
         new_vec_writer%vec = vec_writer%vec
         new_vec_writer%unit_number = unit_number
     end function set_unit_number_to_vec2dcsvwriter
+
+    !>ファイル名に紐付いた装置番号が設定された，スカラ量に対するCSV出力子を返す．
+    function set_unit_number_from_filename_to_scr2dcsvwriter(scr_writer, filename) result(new_scr_writer)
+        use :: grid_uniform_staggered_op_io_unit
+        implicit none
+        !&<
+        type(scalar_2d_csv_writer_type) , intent(in) :: scr_writer
+            !! スカラ量のCSV形式出力子
+        character(*)                    , intent(in) :: filename
+            !! 出力ファイル名
+        !&>
+        type(scalar_2d_csv_writer_type) :: new_scr_writer
+            !! 装置番号が設定されたスカラ量のCSV形式出力子
+
+        new_scr_writer%scr = scr_writer%scr
+        new_scr_writer%unit_number = unit(filename)
+    end function set_unit_number_from_filename_to_scr2dcsvwriter
+
+    !>ファイル名に紐付いた装置番号が設定された，ベクトル量に対するCSV出力子を返す．
+    function set_unit_number_from_filename_to_vec2dcsvwriter(vec_writer, filename) result(new_vec_writer)
+        use :: grid_uniform_staggered_op_io_unit
+        implicit none
+        !&<
+        type(vector_2d_csv_writer_type) , intent(in) :: vec_writer
+            !! ベクトル量のCSV形式出力子
+        character(*)                    , intent(in) :: filename
+            !! 出力ファイル名
+        !&>
+        type(vector_2d_csv_writer_type) :: new_vec_writer
+            !! 装置番号が設定されたベクトル量のCSV形式出力子
+
+        new_vec_writer%vec = vec_writer%vec
+        new_vec_writer%unit_number = unit(filename)
+    end function set_unit_number_from_filename_to_vec2dcsvwriter
 end module grid_uniform_staggered_op_io_to
