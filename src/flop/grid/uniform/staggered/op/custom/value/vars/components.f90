d5bec3a20dff34ec3039b9e025e5dcef0852a8a8
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-07-02 15:37:39 +0900

    added udt for generic interface

---
+++ src/flop/grid/uniform/staggered/op/custom/value/vars/components.f90
@@ -0,0 +1,16 @@
+!>2次元等間隔のスタガード格子に関係する派生型を提供する．
+!>
+!>派生型には，スタガード格子を表す派生型から成分を取得する際に
+!>演算子に所望の成分を認識させる仮の変数が含まれる．
+!>
+module grid_uniform_stg_op_cust_value_vars_components
+    implicit none
+    private
+
+    !>格子幅の最小値．
+    type, public :: minimum_grid_interval_type
+    end type minimum_grid_interval_type
+
+    type(minimum_grid_interval_type), public :: of_minimum_interval
+        !! 演算子に格子幅の最小値を認識させるための仮の変数
+end module grid_uniform_stg_op_cust_value_vars_components
