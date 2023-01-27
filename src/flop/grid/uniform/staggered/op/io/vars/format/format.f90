e3efd0a91db9b4ffb783d86047cab01a15345c80
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-01-27 15:15:00 +0900

    added format specifier for vtr

--- src/flop/grid/uniform/staggered/op/io/vars/format/format.f90
+++ src/flop/grid/uniform/staggered/op/io/vars/format/format.f90
@@ -1,5 +1,6 @@
 !>対応している出力形式を一括して`use`するためのモジュール．
 module grid_uniform_stg_op_io_vars_format
     use :: grid_uniform_stg_op_io_vars_format_csv
+    use :: grid_uniform_stg_op_io_vars_format_vtr
     public
 end module grid_uniform_stg_op_io_vars_format
