dceb7d868d0003b061c2eb2af68e599e50ac113a
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-01-27 18:09:47 +0900

    updated module

--- src/flop/incompressible/op/operators.f90
+++ src/flop/incompressible/op/operators.f90
@@ -6,6 +6,8 @@ module incompressible_op
     use :: incompressible_op_vars_ReynoldsNumber
     use :: incompressible_op_vars_stabilizer_time_advection
     use :: incompressible_op_vars_stabilizer_time_diffusion
+    use :: incompressible_op_vars_stabilizer_time_penalization
+    use :: incompressible_op_vars_stabilizer_characteristics_reluctivity
     use :: incompressible_op_set
     use :: incompressible_op_value
     use :: incompressible_op_by
