88fc82a0bdeaefaf1becccd93027493fc6feb7ee
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-01-27 14:51:12 +0900

    publicized procs related to CG method

--- src/flop/grid/uniform/staggered/op/custom/linearEquations/vars/solver/solver.f90
+++ src/flop/grid/uniform/staggered/op/custom/linearEquations/vars/solver/solver.f90
@@ -3,5 +3,6 @@ module grid_uniform_stg_op_cust_linEqs_vars_solver
     use :: grid_uniform_stg_op_cust_linEqs_vars_solver_adt
     use :: grid_uniform_stg_op_cust_linEqs_vars_solver_lap_SOR
     use :: grid_uniform_stg_op_cust_linEqs_vars_solver_lap_RBSOR
+    use :: grid_uniform_stg_op_cust_linEqs_vars_solver_lap_CG
     public
 end module grid_uniform_stg_op_cust_linEqs_vars_solver
