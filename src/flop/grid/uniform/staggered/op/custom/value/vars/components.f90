17d6016b0abe902f3006305f81b3a725696c39ba
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-01-27 18:05:53 +0900

    fixed docstring

--- src/flop/grid/uniform/staggered/op/custom/value/vars/components.f90
+++ src/flop/grid/uniform/staggered/op/custom/value/vars/components.f90
@@ -7,10 +7,11 @@ module grid_uniform_stg_op_cust_value_vars_components
     implicit none
     private
 
-    !>格子幅の最小値．
+    !>格子幅の最小値を指定するための派生型．
     type, public :: minimum_grid_interval_type
     end type minimum_grid_interval_type
 
-    type(minimum_grid_interval_type), public :: of_minimum_interval
-        !! 演算子に格子幅の最小値を認識させるための仮の変数
+    type(minimum_grid_interval_type), public, parameter :: &
+        of_minimum_interval = minimum_grid_interval_type()
+        !! 演算子に格子幅の最小値を認識させるための定数
 end module grid_uniform_stg_op_cust_value_vars_components
